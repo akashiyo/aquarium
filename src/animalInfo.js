19ddbb4eb43b56ded580c555ffad4bed6ce98ab0
Lucie <monjotlucie@gmail.com>
2026-02-02 11:33:30 +0100

    Remplissage de animalInfo.js

--- src/animalInfo.js
+++ src/animalInfo.js
@@ -11,41 +11,41 @@ export const animalInfo = {
     "jellyfish.glb": {
         number: 2,
         title: "Méduse",
-        text: "<strong> Description : </strong> Les méduses ont un corps mou, en forme de coupelle arrondie : l'ombrelle. Sous l'ombrelle pendent plusieurs tentacules assez fins, généralement cinq, parfois beaucoup plus. Comme beaucoup d'animaux du plancton, la plupart des méduses ont un corps plus ou moins transparent, mais pas toutes. La méduse est aussi dépourvue de cerveau. " +
-            "<br> <strong> Localisation : </strong> Partout dans le monde (mers et océans)" +
-            "<br> <strong> Alimentation : </strong> <ul> <li>Petits animaux planctoniques</li><li> Association avec les zooxanthelles pour se nourrir</li></ul>"
+        text: "<strong> Description : </strong>Les méduses ont un corps mou, en forme de coupelle arrondie : l'ombrelle. Sous l'ombrelle pendent plusieurs tentacules assez fins, généralement cinq, parfois beaucoup plus. Comme beaucoup d'animaux du plancton, la plupart des méduses ont un corps plus ou moins transparent, mais pas toutes. La méduse est aussi dépourvue de cerveau. " +
+            "<br> <strong> Localisation : </strong>Partout dans le monde (mers et océans)." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Petits animaux planctoniques</li><li>Association avec les zooxanthelles pour se nourrir</li></ul>"
     },
 
     "turtle.glb": {
         number: 3,
         title: "Tortue Marine",
         text: " <strong> Description : </strong>Les tortues marines sont des tortues qui vivent des les océans et mers. Elles doivent remonter à la surface pour respirer de l'air et ne viennent à terre que pour se reproduire en pondant leurs œufs qu'elles enterrent dans le sable d'une plage. " +
-            "<br> <strong> Localisation : </strong> Dans tous les océans sauf l'océan Arctique" +
-            "<br> <strong> Alimentation : </strong> <ul> <li>Algues </li><li>Végétaux </li><li>Marins </li><li>Crabes </li><li>Coquillages </li><li>Méduses </li><li> Moules </li><li> Petits poissons</li></ul>"
+            "<br> <strong> Localisation : </strong>Dans tous les océans sauf l'océan Arctique." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Algues </li><li>Végétaux </li><li>Marins </li><li>Crabes </li><li>Coquillages </li><li>Méduses </li><li>Moules </li><li>Petits poissons</li></ul>"
     },
 
     "crab.glb": {
         number: 4,
         title: "Crabe",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        text:  " <strong> Description : </strong>Le crabe est un crustacé marin à corps arrondi, à cinq paires de pattes. Les crabes possèdent huit pattes et deux pinces, plus ou moins grandes selon les espèces, deux yeux et une carapace. " +
+            "<br> <strong> Localisation : </strong>Le long des côtes, dans les estuaires et même dans les fosses océaniques. (Certaines espèces sont adaptées à la vie exclusivement en eau salée, tandis que d’autres prospèrent dans les milieux saumâtres ou d’eau douce)." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Petits crustacés</li><li>Poissons</li> <li>Coquillages</li> <li>Cadavres d'animaux</li></ul>"
     },
 
     "seahorse.glb": {
         number: 5,
-        title: "Hyppocampe",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        title: "Hippocampe",
+        text:  " <strong> Description : </strong>Les hippocampes sont des petits poissons de mer qui nagent en position verticale et dont la tête rabattue contre la gorge rappelle celle d'un cheval." +
+            "<br> <strong> Localisation : </strong>Méditerranée et côtes atlantiques de la péninsule Ibérique et de l'Afrique du Nord." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Petits crustacés</li></ul>"
     },
 
     "fish-nem.glb": {
         number: 6,
         title: "Poisson clown",
-        text:  " <strong> Description : </strong> Les poissons-clowns forment une sous-famille (Amphiprioninae) et de la famille des Pomacentridés. Il en existe une trentaine d'espèces. Les poissons-clowns doivent leur nom à leurs couleurs vives et bariolées : ils sont de couleur orange et certaines espèces ont des bandes blanches. La femelle est plus grosse que le mâle. " +
-            "<br> <strong> Localisation : </strong> Dans toutes les mers et les océans et dans les zones tropicales" +
-            "<br> <strong> Alimentation : </strong> <ul> <li>Petits crustacés</li><li> Larves </li> <li> Algues </li></ul>"
+        text:  " <strong> Description : </strong>Les poissons-clowns forment une sous-famille (Amphiprioninae) et de la famille des Pomacentridés. Il en existe une trentaine d'espèces. Les poissons-clowns doivent leur nom à leurs couleurs vives et bariolées : ils sont de couleur orange et certaines espèces ont des bandes blanches. La femelle est plus grosse que le mâle. " +
+            "<br> <strong> Localisation : </strong>Dans toutes les mers et les océans et dans les zones tropicales." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Petits crustacés</li><li>Larves </li> <li>Algues </li></ul>"
     },
 
     "fishie.glb": {
@@ -59,41 +59,41 @@ export const animalInfo = {
     "koi_fish.glb": {
         number: 8,
         title: "Carpe Koi",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        text:  " <strong> Description : </strong>La carpe koi est un poisson ornemental asiatique. Vivant dans les rizières, ce poisson est le fruit de croisements entre individus de l'espèce appelée Carpe commune, Cyprinus carpio." +
+            "<br> <strong> Localisation : </strong>Dans les eaux tranquilles, chaudes et peu profondes principalemlent en Chine, au Japon, en Corée et au Vietnam." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Crevettes</li><li>Petits crustacés</li> <li>Algues</li> <li>Autres végétaux</li></ul>"
     },
 
     "red_betta_fish.glb": {
         number: 9,
         title: "Poisson combattant",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        text:  " <strong> Description : </strong>Les Betta splendens - ou combattants - sont des poissons tropicaux d'eau douce originaires d'Asie." +
+            "<br> <strong> Localisation : </strong>Dans les eaux stagnantes pauvres en oxygène des plaines inondables, canaux, rizières, et rivières. Principalement de  Thaïlande, Cambodge, Viet Nam, Laos et Malaisie." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Petits crustacés </li><li>Petits insectes</li></ul>"
     },
 
     "animal_crossing_new_horizons_octopus.glb": {
         number: 10,
-        title: " ",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        title: "Poulpe",
+        text:  " <strong> Description : </strong>Le poulpe est un céphalopode sans coquille, qui a huit longs bras égaux munis de ventouses." +
+            "<br> <strong> Localisation : </strong>Dans les eaux tempérées et tropical du monde entirer." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Crustacés</li><li>Bivalves</li> <li>Mollusques</li></ul>"
     },
 
     "octopus.glb": {
         number: 11,
-        title: " ",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        title: "Pieuvre Rouge",
+        text:  " <strong> Description : </strong>La pieuvre rouge du Pacifique oriental est une pieuvre de taille moyenne, de couleur rouge terne à brun rougeâtre, souvent tachetée de blanc . Comme les autres pieuvres, elle peut modifier sa couleur et sa texture, ce qui rend son apparence très variable. Ses bras mesurent généralement trois à quatre fois la longueur de son corps." +
+            "<br> <strong> Localisation : </strong>Dans les eaux mexicaines, son aire de répartition s’étend de la frontière nord jusqu’à Mazatlán, dans l’État de Sinaloa, et comprend tout le golfe de Californie." +
+            "<br> <strong> Alimentation : </strong> <ul><li>Palourdes</li><li>Crabes </li> <li>Pétoncles </li></ul>"
     },
 
     "pelagic_thresher_shark.glb": {
         number: 12,
-        title:  " ",
-        text:  " <strong> Description : </strong>" +
-            "<br> <strong> Localisation : </strong>" +
-            "<br> <strong> Alimentation : </strong> <ul> <li> </li><li> </li> <li> </li></ul>"
+        title:  "Requin Blanc",
+        text:  " <strong> Description : </strong>Le Grand requin blanc est l'un des plus grands prédateurs marins, mesurant typiquement entre 4,5 et 6 mètres de longueur." +
+            "<br> <strong> Localisation : </strong>Dans les eaux côtières et en haute mer, spécialement dans les eaux tempérées des plateaux continentaux de l’Atlantique Nord-Ouest, de la Méditerranée et du Pacifique Nord, et au large des côtes du sud de l’Afrique, du sud de l’Australie, et de la Nouvelle-Zélande." +
+            "<br> <strong> Alimentation : </strong> <ul> <li>Poissons de grandes tailles</li><li>Calmar </li> <li>Tortues marines</li> <li>Otaries</li><li>Phoques</li> <li>Dauphins</li> <li>etc...</li></ul>"
     },
 
     "lowpoly_fish.glb": {
